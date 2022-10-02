10a7faa192d9d4514c073cd15a1b7479d934bd36
Yang Song <entpcoder@gmail.com>
2022-10-02 23:40:07 +0800

    Order 案例

---
+++ service/service_user/src/main/java/com/yiran/user/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.yiran.user.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Yang Song
+ * @date 2022/10/2 23:33
+ */
+@RestController
+public class UserController {
+}
