b10db257f3ef3aeea0e099ee822e03caa201db0b
weiyuwen <3454784839@qq.com>
2022-10-13 08:56:39 +0800

    user 更新

---
+++ service/service_user/src/main/java/com/yiran/user/config/UserConfig.java
@@ -0,0 +1,11 @@
+package com.yiran.user.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.yiran")
+@EnableFeignClients(basePackages = "com.yiran.user")
+public class UserConfig {
+}
