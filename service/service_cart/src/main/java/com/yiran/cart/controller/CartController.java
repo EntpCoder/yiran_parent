3209a824a79c3665a34459d25a37129efddfa6ba
xue <571013506@qq.com>
2022-10-31 11:11:17 +0800

    购物车控制层

---
+++ service/service_cart/src/main/java/com/yiran/cart/controller/CartController.java
@@ -0,0 +1,64 @@
+package com.yiran.cart.controller;
+
+import com.yiran.cart.service.ICartService;
+import com.yiran.common.result.R;
+import com.yiran.model.entity.Cart;
+import com.yiran.model.vo.CartVO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author 小番茄
+ * @date 2022/10/88:38
+ */
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+    private final ICartService cartService;
+    public CartController(ICartService cartService){
+        this.cartService = cartService;
+    }
+    @GetMapping("/userCart/{userId}")
+    public R<List<CartVO>> userCart(@PathVariable("userId") String userId){
+        List<CartVO> cartList = cartService.getUserCart(userId);
+        return cartList == null ? R.fail() : R.ok("cartList",cartList);
+    }
+
+    /**
+     * 购物车添加
+     * @param cart 购物车对象
+     * @return 返回R
+     */
+    @PostMapping("/save")
+    public R<Boolean> saveCart(Cart cart){
+        boolean b = cartService.saveCart(cart);
+        System.out.println(b);
+        return R.ok("",true);
+    }
+
+    /**
+     * 根据购物车id来删除单条购物车里的数据
+     * @param cart 购物车对象
+     * @return R
+     */
+    @DeleteMapping("/deleCart")
+    public R<Boolean> deleCart(Cart cart){
+        boolean b = cartService.deleteCartById(cart.getCartId());
+        System.out.println(b);
+        return R.ok("",true);
+    }
+
+    /**
+     * 根据购物车id来结算订单--批量删除
+     * @param cartIds 购物车id
+     * @return R
+     */
+    @DeleteMapping("/deleaddCart")
+    public R<Boolean> deleaddCart(String[] cartIds){
+        boolean b = cartService.deleteCartByIds(cartIds);
+        System.out.println(b);
+        return R.ok("",true);
+    }
+}
+
