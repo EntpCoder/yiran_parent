8d6f20d9fa85491da6f42931b68294cc7ba77b5c
zzZZ <947091759@qq.com>
2022-11-03 19:12:41 +0800

    Coupon ReceiveCoupon实体类修改 新增优惠券部分接口

--- service/service_coupon/src/main/java/com/yiran/controller/CouponController.java
+++ service/service_coupon/src/main/java/com/yiran/controller/CouponController.java
@@ -22,6 +22,22 @@ public class CouponController {
     public CouponController(CouponService couponService) {
         this.couponService = couponService;
     }
+
+    /**
+     *
+     * 生成优惠券
+     * @param subject 优惠券标题
+     * @param discountAmount 优惠金额
+     * @param fullMoney 满减金额
+     * @param grantStartTime 发放开始时间
+     * @param grandEndTime  发放结束时间
+     * @param usageStartTime 使用时间
+     * @param usageEndTime 使用结束时间
+     * @param timelimit 优惠券时限
+     * @param timeType  时效类型
+     * @param quota 配额
+     * @return  true成功 false失败
+     */
     @PostMapping("/createCoupon")
     public R<Boolean> createCoupon(String subject, String discountAmount, String fullMoney,
                                    LocalDateTime grantStartTime, LocalDateTime grandEndTime,
@@ -30,17 +46,31 @@ public class CouponController {
         return couponService.createCoupon(subject,discountAmount,fullMoney, grantStartTime,grandEndTime,
                 usageStartTime,usageEndTime,timelimit,timeType,quota)? R.ok("创建成功",true):R.fail(ResultCodeEnum.FAIL);
     }
+    /**
+     * 用户查询优惠券
+     * @param userId 用户Id
+     * @return 优惠券集合
+     */
     @GetMapping("/getByUserId")
     public R<List<ReceiveCoupon>> getByUserId(@RequestHeader("userId") String userId){
         List<ReceiveCoupon> couponList = couponService.getByUserId(userId);
         return couponList.size()> 0 ? R.ok("CouponList",couponList):R.fail(ResultCodeEnum.FAIL);
     }
-
+    /**
+     * 通过优惠券id获得优惠金额
+     * @param couponId 优惠券id
+     * @return 优惠金额
+     */
     @GetMapping("/getDiscountAmount/{couponId}")
     public R<BigDecimal> getDiscountAmount(@PathVariable("couponId") String couponId){
         BigDecimal discountAmount = couponService.getDiscountAmount(couponId);
         return discountAmount !=null ? R.ok("discountAmount",discountAmount):R.fail(ResultCodeEnum.FAIL);
     }
+    /**
+     * 根据订单id  改优惠券状态
+     * @param orderId 订单id
+     * @return 优惠券状态
+     */
     @GetMapping("/updateCouponStatus/{orderId}")
     public R<Boolean> updateCouponStatus(@PathVariable("orderId") String orderId){
         return couponService.updateCouponStatus(orderId) ? R.ok("isUpdate",true):R.fail(ResultCodeEnum.FAIL);
