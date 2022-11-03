e43613fd618adaf51e8bc87611c9cce1eacedd94
Yang Song <entpcoder@gmail.com>
2022-11-03 09:19:17 +0800

    优惠券模块

---
+++ service/service_coupon/src/main/java/com/yiran/CouponApplication.java
@@ -0,0 +1,18 @@
+package com.yiran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ * @author Yang Song
+ */
+@SpringBootApplication
+public class CouponApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(CouponApplication.class,args);
+    }
+}
