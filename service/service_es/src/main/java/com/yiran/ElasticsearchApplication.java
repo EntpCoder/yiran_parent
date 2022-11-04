806b738c11f104f3a8c6f77c9be60b46d78b202c
Yang Song <entpcoder@gmail.com>
2022-11-04 18:39:53 +0800

    es模块

---
+++ service/service_es/src/main/java/com/yiran/ElasticsearchApplication.java
@@ -0,0 +1,18 @@
+package com.yiran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ * @author Mr.Song
+ */
+@SpringBootApplication
+public class ElasticsearchApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ElasticsearchApplication.class,args);
+    }
+}
