1e777cb9365efde09c993b4f849e4b5b4af846bc
Yang Song <entpcoder@gmail.com>
2022-11-05 14:27:07 +0800

    mybatis config 分页插件

---
+++ common/service_util/src/main/java/com/yiran/serviceutil/MyBatisPlusConfig.java
@@ -0,0 +1,21 @@
+package com.yiran.serviceutil;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Yang Song
+ * @date 2022/11/5 13:50
+ */
+@Configuration
+public class MyBatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
