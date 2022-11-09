525a75d0a5ddf6b51970a61a0207af27b692fd87
xue <571013506@qq.com>
2022-11-09 09:25:32 +0800

    根据订单id查询单条商品详情远程调用完成

--- service_client/service_user_client/src/main/java/com/yiran/client/user/UserClient.java
+++ service_client/service_user_client/src/main/java/com/yiran/client/user/UserClient.java
@@ -4,7 +4,10 @@ import com.yiran.common.result.R;
 import com.yiran.model.entity.ReceiveAddress;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.websocket.server.PathParam;
 
@@ -15,6 +18,7 @@ import javax.websocket.server.PathParam;
 @FeignClient(name = "service-user")
 @Component
 public interface UserClient {
-    @RequestMapping("/receiveAddress/getAddress")
-    R<ReceiveAddress> getAddress(@PathParam("addressId") String addressId);
+    @GetMapping ("/receiveAddress/getAddress")
+    R<ReceiveAddress> getAddress(@RequestHeader("userId") String usersId,
+                                 @RequestParam("addressId") String addressId);
 }
